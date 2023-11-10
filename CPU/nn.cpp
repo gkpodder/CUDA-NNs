9c2616be91c1989e11d730e87f0b85cd3d876d1a
Gourob Podder <52678361+gkpodder@users.noreply.github.com>
2023-11-09 19:41:03 -0500

    implemented the  forwardpass

--- CPU/nn.cpp
+++ CPU/nn.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <cassert>
+#include <cmath>
 
 using namespace std;
 
@@ -16,22 +18,44 @@ typedef vector<Neuron> Layer;
 class Neuron
 {
 public:
-    Neuron(unsigned numOutputs);
+    Neuron(unsigned numOutputs, unsigned myIndex);
+    void setOutputVal(double val) { m_outputVal = val; }
+    double getOutputVal(void) const { return m_outputVal; }
+    void feedForward(const Layer &prevLayer){};
 
 private:
+    static double activationFunction(double x) { return tanh(x); }
+    static double activationFunctionDerivative(double x) { return 1.0 - x * x; }
     static double randomWeight(void) { return rand() / double(RAND_MAX); }
     double m_outputVal;
     vector<Connection> m_outputWeights;
+    unsigned m_myIndex;
 };
 
-Neuron::Neuron(unsigned numOutputs)
+void Neuron::feedForward(const Layer &prevLayer)
+{
+    double sum = 0.0;
+
+    // Sum the previous layer's outputs (which are our inputs) and include the bias node from the previous layer
+    for (unsigned n = 0; n < prevLayer.size(); ++n)
+    {
+        sum += prevLayer[n].getOutputVal() * prevLayer[n].m_outputWeights[m_myIndex].weight;
+    }
+
+    m_outputVal = Neuron::activationFunction(sum);
+}
+
+Neuron::Neuron(unsigned numOutputs, unsigned myIndex)
 {
     for (unsigned c = 0; c < numOutputs; ++c)
     {
         m_outputWeights.push_back(Connection());
         m_outputWeights.back().weight = randomWeight();
     }
+
+    m_myIndex = myIndex;
 };
+
 class Net
 {
 public:
@@ -44,6 +68,27 @@ private:
     vector<Layer> m_layers; // m_layers[layerNum][neuronNum]
 };
 
+void Net::feedForward(const vector<double> &inputVals)
+{
+    assert(inputVals.size() == m_layers[0].size() - 1);
+
+    // Assign (latch) the input values into the input neurons
+    for (unsigned i = 0; i < inputVals.size(); ++i)
+    {
+        m_layers[0][i].setOutputVal(inputVals[i]);
+    }
+
+    // forward propagate
+    for (unsigned layerNum = 1; layerNum < m_layers.size(); ++layerNum)
+    {
+        Layer &prevLayer = m_layers[layerNum - 1];
+        for (unsigned n = 0; n < m_layers[layerNum].size() - 1; ++n)
+        {
+            m_layers[layerNum][n].feedForward(prevLayer);
+        }
+    }
+}
+
 Net::Net(const vector<unsigned> &topology)
 {
     unsigned numLayers = topology.size();
@@ -55,7 +100,7 @@ Net::Net(const vector<unsigned> &topology)
         // We have made a new Layer, now fill it with neurons, and add a bias neuron to the layer:
         for (unsigned neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum)
         {
-            m_layers.back().push_back(Neuron(numOutputs));
+            m_layers.back().push_back(Neuron(numOutputs, neuronNum));
             cout << "Made a Neuron!" << endl;
         }
     }
